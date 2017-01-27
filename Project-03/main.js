8d2fab285beb825a1762347f34c60cdec2faba6f
Dana <dana.lachman@gmail.com>
2017-01-26 21:22:19 -0600

    blerg. saving changes for the night.

--- Project-03/main.js
+++ Project-03/main.js
@@ -3,13 +3,16 @@ console.log('loooook at the circle', inputs[0].dataset.fun);
 
 function handleUpdate() {
   const sizeSuffix = this.dataset.sizing || '';
-  // console.log(this.name, this.value);
+  sessionStorage.setItem('values', this.value);
   document.documentElement.style.setProperty(`--${this.name}`, this.value + sizeSuffix);
+
 }
 
 // because the collection of inputs isn't actually an array, it's a nodelist.
 // .map() isn't available on the nodelist prototype, but .forEach() is.
 
-
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+// const button = document.querySelector('#testButton');
+// button.addEventListener('click', function() { console.log(inputs[0].value)});
